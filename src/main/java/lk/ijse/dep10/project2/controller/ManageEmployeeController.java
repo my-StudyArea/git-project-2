daa757a1017e59c0c6a996174b4c6c9942bc82b1
tharuka-dilshan <tharukadk329@gmail.com>
2023-03-23 19:23:31 +0530

    fxml done

--- src/main/java/lk/ijse/dep10/project2/controller/ManageEmployeeController.java
+++ src/main/java/lk/ijse/dep10/project2/controller/ManageEmployeeController.java
@@ -1,4 +1,45 @@
 package lk.ijse.dep10.project2.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ManageEmployeeController {
+
+    public Button btnSave;
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNew;
+
+    @FXML
+    private TableView<?> tblEmployees;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
 }
